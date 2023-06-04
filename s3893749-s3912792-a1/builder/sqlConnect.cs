671d6b46535c4c20d6fde66313259cc63c2071a9
karstenbeckqld <s3912792@student.rmit.edu.au>
2023-06-04 22:48:18 +1000

    add database scaffold

--- s3893749-s3912792-a1/builder/sqlConnect.cs
+++ s3893749-s3912792-a1/builder/sqlConnect.cs
@@ -1,6 +1,50 @@
+using Microsoft.Data.SqlClient;
+
 namespace s3893749_s3912792_a1.builder;
 
 public class sqlConnect
 {
-    
+    public sqlConnect()
+    {
+        
+    }
+
+    public void connect()
+    {
+        try
+        {
+            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+            builder.DataSource = "<your_server>.database.windows.net";
+            builder.UserID = "<username>";
+            builder.Password = "<password>";
+            builder.InitialCatalog = "<your_database";
+
+            using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
+            {
+                Console.WriteLine("\nQuery data example:");
+                Console.WriteLine("==========================================");
+
+                String sql = "SELECT name, collation_name FROM sys.databases";
+
+                using (SqlCommand command = new SqlCommand(sql, connection))
+                {
+                    connection.Open();
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Console.WriteLine("{0} {1}", reader.GetString(0), reader.GetString(1));
+                        }
+                    }
+                }
+            }
+        }
+        catch (SqlException e)
+        {
+            Console.WriteLine(e.ToString());
+            throw;
+        }
+
+        Console.ReadLine();
+    }
 }
---
+++ s3893749-s3912792-a1/controller/Controller.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace s3893749_s3912792_a1.controller
+{
+    internal class Controller
+    {
+    }
+}
