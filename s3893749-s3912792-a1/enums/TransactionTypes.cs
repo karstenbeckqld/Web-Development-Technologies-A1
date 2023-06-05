1db6b4f9de3da59514475aa840f1e7aab382e5cc
Karsten Beck <s3912792@student.rmit.edu.au>
2023-06-05 11:45:40 +1000

    add enums

---
+++ s3893749-s3912792-a1/enums/TransactionTypes.cs
@@ -0,0 +1,9 @@
+namespace s3893749_s3912792_a1.enums;
+
+public enum TransactionTypes
+{
+    D=1, // Credit (Deposit money)
+    W=2, // Debit (Withdraw money)
+    T=3, // Credit and Debit (Transferring money between accounts)
+    S=4  // Debit (Service charge)
+}
