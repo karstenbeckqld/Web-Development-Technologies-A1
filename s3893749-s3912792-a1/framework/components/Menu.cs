705b36766a37a2fa569ef8f94ff6f9f8685873b6
Jack Harris <79836947+s3893749@users.noreply.github.com>
2023-06-18 16:21:01 +1000

    - Created Menu component, this component provides a number of choices to the user and will trigger an action based on what one is called.

---
+++ s3893749-s3912792-a1/framework/components/Menu.cs
@@ -0,0 +1,41 @@
+﻿using s3893749_s3912792_a1.framework.core;
+
+namespace s3893749_s3912792_a1.framework.components;
+
+public class Menu : Component
+{
+    private Dictionary<string, string> _options;
+
+    public Menu()
+    {
+        _options = new Dictionary<string, string>();
+    }
+
+    public void AddOption(string name, string method)
+    {
+        _options.Add(name,method);
+    }
+
+    public void AddOption(String name)
+    {
+        _options.Add(name,name);
+    }
+
+    public override Event Process()
+    {
+        
+        Event @event = new Event();
+
+        int count = 0;
+        foreach (var option in _options)
+        {
+            Console.WriteLine("["+count+"] "+option.Key);
+            count++;
+        }
+        
+        //TODO add logic to detect and process the option calls.
+        
+        return null;
+      
+    }
+}
