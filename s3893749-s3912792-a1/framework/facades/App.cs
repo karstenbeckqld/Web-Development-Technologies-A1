f8da5c60c56b176c9bc6cbf60b516aae31c95428
Jack Harris <79836947+s3893749@users.noreply.github.com>
2023-06-18 16:07:44 +1000

    - Created primary App facade, this provides easy access to a number of core App functions.

---
+++ s3893749-s3912792-a1/framework/facades/App.cs
@@ -0,0 +1,37 @@
+﻿using s3893749_s3912792_a1.framework.core;
+
+namespace s3893749_s3912792_a1.framework.facades;
+
+public class App
+{
+    public static void RegisterView(View view)
+    {
+        Kernal.Instance().RegisterView(view);
+    }
+    
+    public static void RegisterView(View view,bool defaultView)
+    {
+        Kernal.Instance().RegisterView(view);
+
+        if (defaultView)
+        {
+            string[] name = view.GetType().FullName.Split(".");
+            Kernal.Instance().SetActiveView(name[name.Length-1]);
+        }
+    }
+
+    public static void ChangeScene(string view)
+    {
+        Kernal.Instance().SetActiveView(view);
+    }
+
+    public static void Start()
+    {
+        Kernal.Instance().Process();
+    }
+
+    public static void LoadConfiguration(string file)
+    {
+        
+    }
+}
