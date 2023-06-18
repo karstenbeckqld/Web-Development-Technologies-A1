ef8daf54f9dba2642005b6d4c8f7ef64a9e971e4
Jack Harris <79836947+s3893749@users.noreply.github.com>
2023-06-18 16:14:09 +1000

    - Created Singleton Kernal class, the kernal is the main beating heart of the program and stores references to our views and calls them as requied by the program.

---
+++ s3893749-s3912792-a1/framework/core/Kernal.cs
@@ -0,0 +1,44 @@
+﻿namespace s3893749_s3912792_a1.framework.core;
+
+public sealed class Kernal
+{
+    private static Kernal _instance = null;
+    private Dictionary<string, View> _views;
+    private string _activeView;
+
+    public Kernal()
+    {
+        _views = new Dictionary<string, View>();
+        _activeView = null;
+    }
+
+    public void RegisterView(View view)
+    {
+        string[] name = view.GetType().FullName.Split(".");
+        _views.Add(name[name.Length-1],view);
+    }
+
+    public static Kernal Instance()
+    {
+        if (_instance == null)
+        {
+            _instance = new Kernal();
+        }
+
+        return _instance;
+    }
+
+    public void Process()
+    {
+        if (_views.ContainsKey(_activeView))
+        {
+            _views[_activeView].Process();
+        }
+    }
+
+    public void SetActiveView(string view)
+    {
+        _activeView = view;
+    }
+    
+}
