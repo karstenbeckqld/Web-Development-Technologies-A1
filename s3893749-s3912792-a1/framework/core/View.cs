75d60e8379a9efede9e2ce4cf466d560a77d499f
Jack Harris <79836947+s3893749@users.noreply.github.com>
2023-06-18 16:27:47 +1000

    - Created the main View parent, this object will be called by the kernal on a process call and process each event in each of its component list.

---
+++ s3893749-s3912792-a1/framework/core/View.cs
@@ -0,0 +1,21 @@
+﻿namespace s3893749_s3912792_a1.framework.core;
+
+public class View
+{
+    protected List<Component> _components;
+
+    public View()
+    {
+        _components = new List<Component>();
+    }
+
+    public void Process()
+    {
+        
+        foreach (var component in _components)
+        {
+            component.Process();
+        }   
+        
+    }
+}
