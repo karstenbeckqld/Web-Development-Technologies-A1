f6352b31a0de51a76c8a2161180c0ff75530fcc3
Jack Harris <79836947+s3893749@users.noreply.github.com>
2023-06-18 16:10:42 +1000

    - Created Component parent class, this provides an abstract process method that returns an event object and accepts a controller string, that it will use to generate a controller object and call its methods.

---
+++ s3893749-s3912792-a1/framework/core/Component.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+
+namespace s3893749_s3912792_a1.framework.core;
+
+public abstract class Component
+{
+    protected object _controller;
+    protected Type _type;
+    
+    public abstract Event Process();
+    
+    public void SetController(string controller)
+    {
+        _type = Type.GetType("s3893749_s3912792_a1.project.controllers." + controller);
+        ConstructorInfo constructorInfo = _type.GetConstructor(Type.EmptyTypes);
+        _controller = constructorInfo.Invoke(new object[] { });
+    }
+    
+    
+}   
