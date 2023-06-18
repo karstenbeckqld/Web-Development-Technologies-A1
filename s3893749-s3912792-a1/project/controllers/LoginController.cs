15bec1f6debb2f40fe4ed969a856f29023acf05b
Jack Harris <79836947+s3893749@users.noreply.github.com>
2023-06-18 17:06:58 +1000

    - Created the LoginController.cs,this impliments our IFormController interface and is called by the Form Component to provide back end functionality.

---
+++ s3893749-s3912792-a1/project/controllers/LoginController.cs
@@ -0,0 +1,33 @@
+﻿using s3893749_s3912792_a1.framework.controllers.interfaces;
+using s3893749_s3912792_a1.framework.core;
+using s3893749_s3912792_a1.framework.facades;
+
+namespace s3893749_s3912792_a1.project.controllers
+{
+    class LoginController : IFormController
+    {
+   
+        public void OnSuccess(Event @event)
+        {
+            App.ChangeScene("MainMenuView");
+        }
+
+        public void OnError(Event @event)
+        {
+            Console.WriteLine("Login failed, please recheck your ID and password!");
+        }
+
+        public bool OnSubmit(Event @event)
+        {
+            
+            if (@event.Get("Login Id") == "12341234")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
