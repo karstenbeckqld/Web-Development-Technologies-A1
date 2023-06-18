017c684fbe71a02f4921acbd9b2a71ca019176ca
Jack Harris <79836947+s3893749@users.noreply.github.com>
2023-06-18 17:06:23 +1000

    - Created the LoginView, this declares how our login form should act and collects login details from the user.

---
+++ s3893749-s3912792-a1/project/views/LoginView.cs
@@ -0,0 +1,19 @@
+﻿using s3893749_s3912792_a1.framework.components;
+using s3893749_s3912792_a1.framework.core;
+
+namespace s3893749_s3912792_a1.project.views;
+
+public class LoginView : View
+{
+    public LoginView()
+    {
+        var form = new Form();
+        
+        form.AddInput(Form.Integer,"Login Id","Please enter your bank login id (8 digits)","char_min:8,char_max:8");
+        form.AddInput(Form.Text,"Password","Please enter your password","regex:password",true);
+        
+        form.SetController("LoginController");
+
+        _components.Add(form);
+    }
+}
