85b5f462216ec7282f9e3aced45e6493a886d59f
Jack Harris <79836947+s3893749@users.noreply.github.com>
2023-06-22 22:33:11 +1000

    - Created Message component, this component will be used to show specific data messages to the user.

---
+++ s3893749-s3912792-a1/framework/components/Message.cs
@@ -0,0 +1,35 @@
+﻿using s3893749_s3912792_a1.framework.core;
+
+namespace s3893749_s3912792_a1.framework.components;
+
+public class Message : Component
+{
+    private string _targetVariable;
+    private ConsoleColor _color;
+
+
+    public void SetVariableKey(string key)
+    {
+        _targetVariable = key;
+    }
+    
+    public override Event Process()
+    {
+        if (_view.GetVariableOrNull(_targetVariable) != null)
+        {
+            if (_color != null)
+            {
+                Console.ForegroundColor = _color;
+            }
+            Console.WriteLine(_view.GetVariableOrNull(_targetVariable));
+            Console.ResetColor();
+        }
+
+        return null;
+    }
+
+    public void SetColor(ConsoleColor color)
+    {
+        _color = color;
+    }
+}
