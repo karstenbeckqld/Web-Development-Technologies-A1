f7aa1abe49f796a12dfe758c55b6be778b44d0bc
Jack Harris <79836947+s3893749@users.noreply.github.com>
2023-06-22 22:29:12 +1000

    - Added _view reference to the Component class, this allows them to access view variables.

--- s3893749-s3912792-a1/framework/core/Component.cs
+++ s3893749-s3912792-a1/framework/core/Component.cs
@@ -6,6 +6,7 @@ public abstract class Component
 {
     protected object _controller;
     protected Type _type;
+    protected View _view;
     
     public abstract Event Process();
     
@@ -14,6 +15,11 @@ public abstract class Component
         _type = Type.GetType("s3893749_s3912792_a1.project.controllers." + controller);
         ConstructorInfo constructorInfo = _type.GetConstructor(Type.EmptyTypes);
         _controller = constructorInfo.Invoke(new object[] { });
+    }
+
+    public void SetView(View view)
+    {
+        _view = view;
     }
     
     
