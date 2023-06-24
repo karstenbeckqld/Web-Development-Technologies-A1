153f9af0defa552f09604d68df18f2902383058d
Jack Harris <79836947+s3893749@users.noreply.github.com>
2023-06-24 22:33:15 +1000

    - Files deleted from Class library refactoring

--- s3893749-s3912792-a1/framework/controllers/interfaces/IMenuController.cs
+++
@@ -1,8 +0,0 @@
-﻿using s3893749_s3912792_a1.framework.core;
-
-namespace s3893749_s3912792_a1.framework.controllers.interfaces;
-
-public interface IMenuController
-{
-    void OnError(Event @event);
-}
