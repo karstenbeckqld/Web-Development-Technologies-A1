d9bd428f4de627a9952d04a10996bc023bb8a51e
Jack Harris <79836947+s3893749@users.noreply.github.com>
2023-06-24 21:59:42 +1000

    - Created DatabaseConfigurator class, this class will hold any database configuration that needs to be set, currently it only accepts a database uri.

---
+++ EasyDB/DatabaseConfigurator.cs
@@ -0,0 +1,16 @@
+﻿namespace Database;
+
+public class DatabaseConfigurator
+{
+    private static string _uri;
+    
+    public static void SetDatabaseURI(string uri)
+    {
+        _uri = uri;
+    }
+
+    public static string GetDatabaseURI()
+    {
+        return _uri;
+    }
+}
