d00556c7578287f5127a4be04cb64ef5f2d07312
Jack Harris <79836947+s3893749@users.noreply.github.com>
2023-06-26 22:56:31 +1000

    - Created the AccountSelectionView, this view is called before the MyStatementsView and is used to get the account selection from the user.

---
+++ MyBank/project/views/AccountSelectionView.cs
@@ -0,0 +1,38 @@
+﻿using EasyDB.core;
+using MyBank.framework.components;
+using MyBank.framework.core;
+using MyBank.framework.facades;
+using MyBank.framework.views.interfaces;
+using MyBank.project.models;
+
+namespace MyBank.project.views;
+
+public class AccountSelectionView : View, IDefeeredConstructor
+{
+
+    public void Construct()
+    {
+        
+        var message = new Message();
+        
+        message.SetContent("Please select an account, S = Savings, C = Credit");
+        message.SetColor(ConsoleColor.Yellow);
+        
+        AddComponent(message);
+        
+        
+        var accountMenu = new Menu<Account>();
+
+        List<Account> accounts = new Database<Account>().Where("CustomerID", App.GetCurrentUser().CustomerID.ToString())
+            .GetAll();
+        
+        accountMenu.AddAll(accounts,"AccountType","AccountNumber");
+        
+        accountMenu.SetController("AccountSelectionController");
+        
+        accountMenu.AddOption("Back");
+        
+        AddComponent(accountMenu);
+
+    }
+}
