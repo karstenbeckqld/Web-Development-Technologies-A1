913907e77984fbaa5d33d6fa1bf6a0dc43d9723b
Jack Harris <79836947+s3893749@users.noreply.github.com>
2023-06-26 22:55:36 +1000

    - Created the Account Selection Controller, this has two methods that match the users accounts, NOTE: its not possible for the user to trigger a method if it is excluded from the front end.

---
+++ MyBank/project/controllers/AccountSelectionController.cs
@@ -0,0 +1,36 @@
+﻿using EasyDB.core;
+using MyBank.framework.core;
+using MyBank.framework.facades;
+using MyBank.project.models;
+
+namespace MyBank.project.controllers;
+
+public class AccountSelectionController
+{
+    public void S(Event @event)
+    {
+        Console.WriteLine( @event.Get("MenuSelectionValue"));
+
+        Account account = new Database<Account>().Where("AccountNumber", @event.Get("MenuSelectionValue")).GetFirst();
+        
+        App.SetViewVariable("MyStatementsView","Account","Account: "+account.AccountNumber+", Balance: $"+account.Balance);
+        App.SetViewVariable("MyStatementsView","Heading","My Statement | Savings");
+        
+        App.SwitchView("MyStatementsView");
+    }
+
+    public void C(Event @event)
+    {
+        Account account = new Database<Account>().Where("AccountNumber", @event.Get("MenuSelectionValue")).GetFirst();
+        
+        App.SetViewVariable("MyStatementsView","Account","Account: "+account.AccountNumber+", Balance: $"+account.Balance);
+        App.SetViewVariable("MyStatementsView","Heading","My Statement | Credit");
+        
+        App.SwitchView("MyStatementsView");
+    }
+
+    public void Back()
+    {
+        App.SwitchView("MainMenuView");
+    }
+}
