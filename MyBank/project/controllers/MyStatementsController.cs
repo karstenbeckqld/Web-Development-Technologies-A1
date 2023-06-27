477a0761dc43d9fddeea041ea577cfdc724be376
Jack Harris <79836947+s3893749@users.noreply.github.com>
2023-06-27 20:30:35 +1000

    - Created MyStatementsView & MyStatementsController

---
+++ MyBank/project/controllers/MyStatementsController.cs
@@ -0,0 +1,12 @@
+﻿using MyBank.framework.facades;
+
+namespace MyBank.project.controllers;
+
+public class MyStatementsController
+{
+    public void Back()
+    {
+        App.SwitchView("MainMenuView");
+    }
+    
+}
---
+++ MyBank/project/views/MyStatementsView.cs
@@ -0,0 +1,32 @@
+﻿using System.Drawing;
+using MyBank.framework.components;
+using MyBank.framework.core;
+using MyBank.framework.views.interfaces;
+
+namespace MyBank.project.views;
+
+public class MyStatementsView : View, IDefeeredConstructor
+{
+
+    public void Construct()
+    {
+        
+        var heading = new Message();
+        
+        heading.SetVariableKey("Heading");
+        heading.SetColor(ConsoleColor.Yellow);
+        AddComponent(heading);
+
+        var account = new Message();
+        
+        account.SetVariableKey("Account");
+        account.SetColor(ConsoleColor.DarkYellow);
+        AddComponent(account);
+
+        var menu = new Menu<string>();
+        menu.AddOption("Back");
+        menu.SetController("MyStatementsController");
+            
+        AddComponent(menu);
+    }
+}
