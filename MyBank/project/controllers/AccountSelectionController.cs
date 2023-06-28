702f01fa28e111e2ca21e9c7bee258058560cb98
Jack Harris <79836947+s3893749@users.noreply.github.com>
2023-06-28 22:22:51 +1000

    - Added Available balance to Statement header.

--- MyBank/project/controllers/AccountSelectionController.cs
+++ MyBank/project/controllers/AccountSelectionController.cs
@@ -13,7 +13,7 @@ public class AccountSelectionController
         Account account = new Database<Account>().Where("AccountNumber", @event.Get("MenuSelectionValue")).GetAll()
             .GetResult()[0];
         
-        App.SetViewVariable("MyStatementsView","Account","Account: "+account.AccountNumber+", Balance: $"+account.Balance);
+        App.SetViewVariable("MyStatementsView","Account","Account: "+account.AccountNumber+", Balance: "+((float)account.Balance).ToString("c2")+", Available Balance: "+((float)account.Balance-0).ToString("c2"));
         App.SetViewVariable("MyStatementsView","Heading","My Statement | Savings");
         App.SetViewVariable("MyStatementsView","AccountNumber",account.AccountNumber);
         App.SetViewVariable("MyStatementsView","SavingsTransactions",new Database<Transaction>().Where("AccountNumber",account.AccountNumber.ToString()).GetAll().GetResult());
@@ -26,7 +26,7 @@ public class AccountSelectionController
         Account account = new Database<Account>().Where("AccountNumber", @event.Get("MenuSelectionValue")).GetAll()
             .GetResult()[0];
         
-        App.SetViewVariable("MyStatementsView","Account","Account: "+account.AccountNumber+", Balance: $"+account.Balance);
+        App.SetViewVariable("MyStatementsView","Account","Account: "+account.AccountNumber+", Balance: "+((float)account.Balance).ToString("c2")+", Available Balance: "+((float)account.Balance-300).ToString("c2"));
         App.SetViewVariable("MyStatementsView","Heading","My Statement | Credit");
         App.SetViewVariable("MyStatementsView","AccountNumber",account.AccountNumber);
         App.SetViewVariable("MyStatementsView","CreditTransactions",new Database<Transaction>().Where("AccountNumber",account.AccountNumber.ToString()).GetAll().GetResult());
