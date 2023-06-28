46f606900d0ee3d6bbfdf69b8702d7eb332269e6
Jack Harris <79836947+s3893749@users.noreply.github.com>
2023-06-28 18:35:30 +1000

    - Created new table component.

---
+++ MyBank/framework/components/Table.cs
@@ -0,0 +1,25 @@
+﻿using MyBank.framework.core;
+using MyBank.project.models;
+
+namespace MyBank.framework.components;
+
+public class Table<T> : Component
+{
+    
+    private List<T> _values;
+    private string _displayKey;
+    
+    public void SetVariableKey(string key)
+    {
+        _displayKey = key;
+    }
+    public override Event Process()
+    {
+        foreach (var value in (List<Transaction>)_view.GetVariableOrNull(_displayKey))
+        {
+            Console.WriteLine(value.AccountNumber);
+        }
+        
+        return null;
+    }
+}
