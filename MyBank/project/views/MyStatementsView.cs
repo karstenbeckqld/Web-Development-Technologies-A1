8130cd3f0eae55bb64df36c5cd0c6fc0d6fbd0fd
Jack Harris <79836947+s3893749@users.noreply.github.com>
2023-06-28 18:35:05 +1000

    - Added table to MyStatementsView

--- MyBank/project/views/MyStatementsView.cs
+++ MyBank/project/views/MyStatementsView.cs
@@ -1,7 +1,9 @@
 ﻿using System.Drawing;
+using EasyDB.core;
 using MyBank.framework.components;
 using MyBank.framework.core;
 using MyBank.framework.views.interfaces;
+using MyBank.project.models;
 
 namespace MyBank.project.views;
 
@@ -28,5 +30,11 @@ public class MyStatementsView : View, IDefeeredConstructor
         menu.SetController("MyStatementsController");
             
         AddComponent(menu);
+
+        var table = new Table<Transaction>();
+        
+        table.SetVariableKey("Transactions");
+        
+        AddComponent(table);
     }
 }
