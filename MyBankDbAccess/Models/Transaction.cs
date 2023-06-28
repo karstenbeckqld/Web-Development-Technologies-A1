4584d1078c4e2ad7e4a46cf6a686b9c64474ad9d
Jack Harris <79836947+s3893749@users.noreply.github.com>
2023-06-28 22:14:17 +1000

    - Rebuild Transaction.cs class to output a format that matches the table size and specifications.

--- MyBankDbAccess/Models/Transaction.cs
+++ MyBankDbAccess/Models/Transaction.cs
@@ -33,13 +33,58 @@ public class Transaction
 
     public override string ToString()
     {
-        return $"TransactionID: {TransactionID} - " +
-               $"TransactionType: {TransactionType} - " +
-               $"AccountNumber: {AccountNumber} - " +
-               $"DestinationAccountNumber: {DestinationAccountNumber} - " +
-               $"Amount: {Amount} - " +
-               $"Comment: {Comment} - " +
-               $"TransactionTimeUtc: {TransactionTimeUtc}";
+
+        string output = "|";
+
+        output += FixedLength(TransactionID.ToString(), 4, ' ');
+        output += "|";
+
+        switch (TransactionType)
+        {
+            case "D":
+                output += FixedLength("Deposit", 14, ' ');
+                break;
+            case "W":
+                output += FixedLength("Withdraw", 14, ' ');
+                break;
+            case "T":
+                output += FixedLength("Transfer", 14, ' ');
+                break;
+            case "S":
+                output += FixedLength("Service Charge", 14, ' ');
+                break;
+        }
+        output += "|";
+
+        output += FixedLength(Amount.ToString("c2"), 10, ' ');
+        
+        output += "|";
+
+        output += FixedLength(DestinationAccountNumber.ToString(),20,' ') + "|";
+
+        output += FixedLength(Comment, 32, ' ');
+        
+        output += "|";
+
+        output += FixedLength(TransactionTimeUtc.ToString(),23,' ')+"|";
+
+        return output;
+    }
+    
+    //Reference for method "FixedLength"
+    
+    //Panoch, J. and wakawaka (2022) How to truncate or pad a string to a fixed length in C#,
+    //Stack Overflow. Available at: https://stackoverflow.com/questions/43096857/how-to-truncate-or-pad-a-string-to-a-fixed-length-in-c-sharp
+    //(Accessed: 28 June 2023). 
+    public static string FixedLength(string value, int totalWidth, char paddingChar)
+    {
+        if (value is null)
+            return new string(paddingChar, totalWidth);
+
+        if (value.Length > totalWidth)
+            return value.Substring(0, totalWidth);
+        else
+            return value.PadRight(totalWidth, paddingChar);
     }
 
   
