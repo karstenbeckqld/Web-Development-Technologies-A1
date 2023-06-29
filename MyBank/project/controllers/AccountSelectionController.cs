a0f89fc7bf2bf0c144dae781ba99940393217e76
Jack Harris <79836947+s3893749@users.noreply.github.com>
2023-06-29 22:54:16 +1000

    - Combined both S & C methods into one process method. - Added functionality for the AccountSelectionController to now detect a Redirect key and redirect to that specific view.

--- MyBank/project/controllers/AccountSelectionController.cs
+++ MyBank/project/controllers/AccountSelectionController.cs
@@ -7,31 +7,62 @@ namespace MyBank.project.controllers;
 
 public class AccountSelectionController
 {
+    //As we share the same code for both the savings and 
+    //credit account we can simply map these menu operations
+    //to a generic process method.
     public void S(Event @event)
     {
-
-        Account account = new Database<Account>().Where("AccountNumber", @event.Get("MenuSelectionValue")).GetAll()
-            .GetResult()[0];
-        
-        App.SetViewVariable("MyStatementsView","Account","Account: "+account.AccountNumber+", Balance: "+((float)account.Balance).ToString("c2")+", Available Balance: "+((float)account.Balance-0).ToString("c2"));
-        App.SetViewVariable("MyStatementsView","Heading","My Statement | Savings");
-        App.SetViewVariable("MyStatementsView","AccountNumber",account.AccountNumber);
-        App.SetViewVariable("MyStatementsView","SavingsTransactions",new Database<Transaction>().Where("AccountNumber",account.AccountNumber.ToString()).GetAll().GetResult());
-
-        App.SwitchView("MyStatementsView");
+        Process(@event);
     }
 
     public void C(Event @event)
     {
+        Process(@event);
+    }
+
+
+    public void Process(Event @event)
+    {
+
+        //If we have no redirect simply return.
+        if (App.GetViewVariable("AccountSelectionView", "Redirect") == null)
+        {
+            return;
+        }
+
+        //Next we get the account that has been selected
+        //We do not need to validate this as the menu
+        //will only display valid accounts.
         Account account = new Database<Account>().Where("AccountNumber", @event.Get("MenuSelectionValue")).GetAll()
             .GetResult()[0];
         
-        App.SetViewVariable("MyStatementsView","Account","Account: "+account.AccountNumber+", Balance: "+((float)account.Balance).ToString("c2")+", Available Balance: "+((float)account.Balance-300).ToString("c2"));
-        App.SetViewVariable("MyStatementsView","Heading","My Statement | Credit");
-        App.SetViewVariable("MyStatementsView","AccountNumber",account.AccountNumber);
-        App.SetViewVariable("MyStatementsView","CreditTransactions",new Database<Transaction>().Where("AccountNumber",account.AccountNumber.ToString()).GetAll().GetResult());
-
-        App.SwitchView("MyStatementsView");
+        //Get our account type.
+        string accountType = account.AccountType.Equals("S") ? "Savings" : "Credit";
+        
+        //Perform our logic
+        switch (App.GetViewVariable("AccountSelectionView", "Redirect"))
+        {
+                
+            case "MyStatementsView":
+                
+                App.SetViewVariable("MyStatementsView", "Account", "Account: " + account.AccountNumber + ", Balance: " + ((float)account.Balance).ToString("c2") +
+                                                                   ", Available Balance: " + ((float)account.Balance - 0).ToString("c2"));
+                App.SetViewVariable("MyStatementsView", "Heading", "My Statement | "+accountType);
+                App.SetViewVariable("MyStatementsView", "AccountNumber", account.AccountNumber);
+                App.SetViewVariable("MyStatementsView", accountType+"Transactions",
+                        new Database<Transaction>().Where("AccountNumber", account.AccountNumber.ToString()).GetAll()
+                            .GetResult());
+                break;
+                
+            //If we are redirecting to deposit process this code.
+            case "DepositView":
+                App.SetViewVariable("DepositView", "AccountNumber", account.AccountNumber);
+                break;
+        }
+            
+            //Finally we clear the redirect variable and redirect to that view.
+            string view = App.GetViewVariable("AccountSelectionView", "Redirect").ToString();
+            App.SwitchView(view);
     }
 
     public void Back()
