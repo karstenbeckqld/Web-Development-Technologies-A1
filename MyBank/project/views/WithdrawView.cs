538d511a0decb735aadaf922e66e943e1bd76cbe
Jack Harris <79836947+s3893749@users.noreply.github.com>
2023-07-01 20:59:49 +1000

    - Created the WithdrawView, this view requests the user enter the withdraw amount followed by an option comment.

---
+++ MyBank/project/views/WithdrawView.cs
@@ -0,0 +1,19 @@
+﻿using MyBank.framework.components;
+using MyBank.framework.core;
+
+namespace MyBank.project.views;
+
+public class WithdrawView : View
+{
+    public WithdrawView()
+    {
+        var getDepositAmountFromUser = new Form();
+        
+        getDepositAmountFromUser.AddInput(Form.Float,"Amount","Please enter the amount you would like to withdraw.","min:1");
+        getDepositAmountFromUser.AddInput(Form.Text,"Comment","If you would like to enter a comment please enter it now\nor simple press enter to skip",null);
+        
+        getDepositAmountFromUser.SetController("WithdrawController");
+        
+        AddComponent(getDepositAmountFromUser);    
+    }
+}
