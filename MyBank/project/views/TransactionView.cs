377bdc5f34691a4ef6324f740f6d596e953780c0
Jack Harris <79836947+s3893749@users.noreply.github.com>
2023-07-01 21:49:21 +1000

    - Created and programmed the TransactionView and its input form.

---
+++ MyBank/project/views/TransactionView.cs
@@ -0,0 +1,24 @@
+﻿using MyBank.framework.components;
+using MyBank.framework.core;
+
+namespace MyBank.project.views;
+
+public class TransactionView: View
+{
+
+    public TransactionView()
+    {
+
+        var form = new Form();
+        
+        form.AddInput(Form.Float,"Amount","Please enter the amount you would like to transfer","min:1");
+        form.AddInput(Form.Text,"Comment","If you would like to enter a comment please enter it now\nor simple press enter to skip",null);
+        form.AddInput(Form.Integer,"Destination Account","Please enter the destination account you wish to transfer to.","char_min:4,char_max:4");
+        
+        form.SetController("TransactionController");
+        
+        AddComponent(form);
+    }
+    
+    
+}
