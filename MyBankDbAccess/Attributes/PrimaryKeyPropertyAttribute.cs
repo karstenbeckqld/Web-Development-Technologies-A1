b713f93cc34d6d4d1766381ceda5a2a8bd458d00
Jack Harris <79836947+s3893749@users.noreply.github.com>
2023-07-02 12:05:14 +1000

    - Deleted un-used classed and old folder.

--- MyBankDbAccess/Attributes/PrimaryKeyPropertyAttribute.cs
+++
@@ -1,6 +0,0 @@
-namespace MyBankDbAccess.attributes;
-
-// The PrimaryKeyAttribute is a class that extends Attribute and marks properties that are primary keys where necessary.
-public class PrimaryKeyAttribute:Attribute
-{
-}
--- MyBankDbAccess/Utils/DbConnectionString.cs
+++
@@ -1,9 +0,0 @@
-namespace MyBankDatabase.Utils;
-
-// CAN THIS CLASS GO?
-
-
-public static class DbConnectionString
-{
-    public static string DbConnect { get; set; }
-}
