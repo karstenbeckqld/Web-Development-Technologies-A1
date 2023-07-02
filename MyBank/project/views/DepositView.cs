e15507f750972111a19cfa20eaf19e500444d466
Jack Harris <79836947+s3893749@users.noreply.github.com>
2023-07-02 11:46:28 +1000

    - Updated min to 0.01

--- MyBank/project/views/DepositView.cs
+++ MyBank/project/views/DepositView.cs
@@ -9,7 +9,7 @@ public class DepositView : View
     {
         var getDepositAmountFromUser = new Form();
         
-        getDepositAmountFromUser.AddInput(Form.Float,"Amount","Please enter the amount you would like to deposit too.","min:1");
+        getDepositAmountFromUser.AddInput(Form.Float,"Amount","Please enter the amount you would like to deposit too.","min:0.01");
         getDepositAmountFromUser.AddInput(Form.Text,"Comment","If you would like to enter a comment please enter it now\nor simple press enter to skip",null);
         
         getDepositAmountFromUser.SetController("DepositController");
